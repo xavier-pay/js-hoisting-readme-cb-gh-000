0fb4f563bb5202187bcd8de1e1c38e8f44e81913
Aurelien Xavier Payong D <xavier.payong@gmail.com>
2018-01-12 19:37:24 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -23,11 +23,10 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-
-
   function sayMy() {
-    console.log(name);
+    
     var name = "Kristin";
+    console.log(name);
   }
   sayMy();
 }
